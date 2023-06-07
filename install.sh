729cc74923f968e8386bb9218908e82006fdec0b
sarthhh <shiva02939@gmail.com>
2023-06-07 08:39:07 +0530

    add core configs

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+
+sudo apt update
+sudo apt install alacritty zsh ripgrep neovim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# setup zsh as default terminal
+chsh -s $(which zsh)
